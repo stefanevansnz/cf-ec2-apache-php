1bb980ecdbe22e8b63f3baf29465f0cca293bf37
Stefan Evans <stefan.evans@consegna.cloud>
2018-02-10 17:58:28 +1300

    added install

--- run_stack.sh
+++ run_stack.sh
@@ -1,4 +1,5 @@
-#create stack
-#aws cloudformation create-stack --template-body file://apache-php-instance.yml --stack-name apache-php-instance --parameters ParameterKey=KeyName,ParameterValue=MyEC2KeyPair2 ParameterKey=InstanceType,ParameterValue=t2.nano
 #delete stack
 #aws cloudformation delete-stack --stack-name apache-php-instance
+
+#create stack
+aws cloudformation create-stack --template-body file://apache-php-instance.yml --stack-name apache-php-instance --parameters ParameterKey=KeyName,ParameterValue=MyEC2KeyPair2 ParameterKey=InstanceType,ParameterValue=t2.nano
