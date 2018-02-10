a8194874f38868a32816b3bfd330517365655c49
Stefan Evans <stefan.evans@consegna.cloud>
2018-02-10 15:20:28 +1300

    initial stack

---
+++ run_stack.sh
@@ -0,0 +1,4 @@
+#create stack
+#aws cloudformation create-stack --template-body file://apache-php-instance.yml --stack-name apache-php-instance --parameters ParameterKey=KeyName,ParameterValue=MyEC2KeyPair2 ParameterKey=InstanceType,ParameterValue=t2.nano
+#delete stack
+#aws cloudformation delete-stack --stack-name apache-php-instance
